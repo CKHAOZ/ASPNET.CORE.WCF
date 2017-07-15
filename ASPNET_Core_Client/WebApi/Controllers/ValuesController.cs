c8d6a40a326d9314ee28c2b6f0830554068acd96
Bryant Alejandro Cubillos Prieto <Bryant Alejandro Cubillos Prieto>
2017-07-14 19:00:17 -0500

    Consumiendo WCF desde ASPNET Core

---
+++ ASPNET_Core_Client/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using service;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //Service1Client client = new Service1Client();
+            //client.
+
+            //ServiceRe.Service1Client clien = new ServiceRe.Service1Client();
+            //var ff =  clien.GetDataAsync(new ServiceRe.GetDataRequest() { value = 654 });
+
+            ServiceReferenceOne.Service1Client cli = new ServiceReferenceOne.Service1Client();
+
+            string data = cli.GetData(7845);
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WCF/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference.Service1Client cli = new ServiceReference.Service1Client();
+
+            string data = cli.GetData(7845);
+        }
+    }
+}
